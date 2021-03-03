12b7a98e531838fa58bebdb62b5ffd2e2a6ffd1c
Mike <mikewrubel51@gmail.com>
2021-03-03 16:20:56 -0500

    commit tree b0085a1712da51464494ccfbaf3c86846f06cac1

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = []
+def line(line_length)
+    if line_length >= 1
+        return line_length
+    else
+        puts "The line is currently empty"
+    end
+end
+
+def take_a_number(katz_deli, new_name)
+
+end
+
+def now_serving
+    if line(line_length) >= 1
+        puts ""
+    else
+        puts "There is nobody waiting to be served!"
+    end
+end
